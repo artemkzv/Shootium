f8655a97c22fba9b59d4efc737b5b7e55b816aca
Artem <artemkuzov@yandex.ru>
2023-12-22 20:57:50 +0300

    Spawn AICharacter

--- Source/Shootium/Private/ShootiumGameModeBase.cpp
+++ Source/Shootium/Private/ShootiumGameModeBase.cpp
@@ -5,10 +5,42 @@
 #include "Player/ShootiumBaseCharacter.h"
 #include "Player/ShootiumPlayerController.h"
 #include "UI/ShootiumGameHUD.h"
+#include "AIController.h"
 
 AShootiumGameModeBase::AShootiumGameModeBase()
 {
     DefaultPawnClass = AShootiumBaseCharacter::StaticClass();
     PlayerControllerClass = AShootiumPlayerController::StaticClass();
     HUDClass = AShootiumGameHUD::StaticClass();
+}
+
+void AShootiumGameModeBase::StartPlay() 
+{
+    Super::StartPlay();
+
+    SpawnBots();
+}
+
+UClass* AShootiumGameModeBase::GetDefaultPawnClassForController_Implementation(AController* InController)
+{
+    if (InController && InController->IsA<AAIController>())
+    {
+        return AIPawnClass;
+    }
+    return Super::GetDefaultPawnClassForController_Implementation(InController);
+}
+
+void AShootiumGameModeBase::SpawnBots() 
+{
+    if (!GetWorld())
+        return;
+
+    for (int32 i = 0; i < GameData.PlayerNum - 1; ++i)
+    {
+        FActorSpawnParameters SpawnInfo;
+        SpawnInfo.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+        const auto ShootiumAIController = GetWorld()->SpawnActor<AAIController>(AIControllerClass, SpawnInfo);
+        RestartPlayer(ShootiumAIController);
+    }
 }
--- Source/Shootium/Public/ShootiumGameModeBase.h
+++ Source/Shootium/Public/ShootiumGameModeBase.h
@@ -4,16 +4,32 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "ShootiumCoreTypes.h"
 #include "ShootiumGameModeBase.generated.h"
 
-/**
- * 
- */
+class AAIController;
+
 UCLASS()
 class SHOOTIUM_API AShootiumGameModeBase : public AGameModeBase
 {
-	GENERATED_BODY()
-	
-	public:
+    GENERATED_BODY()
+
+public:
     AShootiumGameModeBase();
+
+    virtual void StartPlay() override;
+    virtual UClass* GetDefaultPawnClassForController_Implementation(AController* InController) override;
+
+protected:
+    UPROPERTY(EditDefaultsOnly, Category = "Game")
+    TSubclassOf<AAIController> AIControllerClass;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Game")
+    TSubclassOf<APawn> AIPawnClass;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Game")
+    FGameData GameData;
+
+private:
+    void SpawnBots();
 };
